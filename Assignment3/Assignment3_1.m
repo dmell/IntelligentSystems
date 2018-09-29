864f45ac97d0a403b8a159f52501087d99cd17e0
dmell <damiano.melotti@studenti.unitn.it>
2018-09-29 14:34:07 +0200

    Work for Assignment 3

---
+++ Assignment3/Assignment3_1.m
@@ -0,0 +1,46 @@
+% What we have to calculate is: knowing that x is the length of the fish,
+% give me the probability of that fish to be a seabass (salmon).
+% The data we have is: given that this fish is a seabass, p_seabass(i) gives
+% the percentage of seabass that have length i.
+
+load("lab_week3_data/lab3_1.mat");
+N = numel(p_salmon);
+% "Given that sea bass is caught 3 times as often as salmon"
+prior_salmon = 0.25;
+prior_seabass = 0.75;
+
+posterior_salmon = zeros(N);
+posterior_seabass = zeros(N);
+
+for i=1:N
+    % evidence
+    p_x = p_salmon(i) * prior_salmon + p_seabass(i) * prior_seabass;
+    % Bayes formula for both sets
+    posterior_salmon(i) = (p_salmon(i) * prior_salmon)/p_x;
+    posterior_seabass(i) = (p_seabass(i) * prior_seabass)/p_x;
+end
+
+p1 = plot(l, posterior_salmon, 'color', 'r');
+hold on;
+p2 = plot(l, posterior_seabass, 'color', 'b');
+legend([p1(1), p2(1)], 'P(salmon | x)', 'P(seabass | x)');
+title('Posterior probabilities');
+xlabel('Length (cm)');
+ylabel('Probability');
+set(gca, 'fontsize', 8.5, 'fontname', 'Times New Roman');
+
+if (posterior_seabass(8) > posterior_salmon(8))
+    fprintf(['The fish with length 8 cm can be classified as a seabass, ', ...
+        'with a probability of %f\n'], posterior_seabass(8));
+else
+    fprintf(['The fish with length 8 cm can be classified as a salmon, ', ...
+        'with a probability of %f\n'], posterior_salmon(8));
+end
+
+if (posterior_seabass(20) > posterior_salmon(20))
+    fprintf(['The fish with length 20 cm can be classified as a seabass, ', ...
+        'with a probability of %f\n'], posterior_seabass(20));
+else
+    fprintf(['The fish with length 20 cm can be classified as a salmon, ', ...
+        'with a probability of %f\n'], posterior_salmon(20));
+end
---
+++ Assignment3/Assignment3_2.m
@@ -0,0 +1,66 @@
+load("lab_week3_data/normdist.mat");
+
+% 2.1
+figure(1);
+p1 = plot(S1, 0, '-bo');
+hold on;
+p2 = plot(S2, 0, '-ro');
+p3 = plot(T, 0, '-ks');
+legend([p1(1), p2(1), p3(1)], 'S1', 'S2', 'T');
+title('Elements of S1, S2 and T');
+xlabel('S1, S2, T');
+ylabel('  ');
+set(gca, 'fontsize', 8.5, 'fontname', 'Times New Roman');
+
+% 2.2
+% maximum estimation
+mean1 = mean(S1);
+dev1 = std(S1);
+mean2 = mean(S2);
+dev2 = std(S2);
+
+figure(2);
+% x values to calculate the normal distributions
+X = [-40:1:100];
+p1 = plot(X, normpdf(X, mean1, dev1), 'color', 'b');
+hold on;
+p2 = plot(X, normpdf(X, mean2, dev2), 'color', 'r');
+p3 = plot(S1, 0, '-bo');
+p4 = plot(S2, 0, '-ro');
+legend([p1(1), p2(1), p3(1), p4(1)], 'p(x|\omega_1)', 'p(x|\omega_2)', ...
+    'S1', 'S2','location', 'northwest');
+title('Gaussian functions of S1 and S2');
+xlabel('S1, S2');
+ylabel('Probability');
+set(gca, 'fontsize', 8.5, 'fontname', 'Times New Roman');
+
+% 2.3
+total = numel(S1) + numel(S2);
+prior_S1 = numel(S1)/total;
+prior_S2 = numel(S2)/total;
+fprintf('The prior probability of S1 is %f\n', prior_S1);
+fprintf('The prior probability of S2 is %f\n', prior_S2);
+
+% 2.4
+figure(3);
+p1 = plot(X, normpdf(X, mean1, dev1)*prior_S1, 'color', 'b');
+hold on;
+p2 = plot(X, normpdf(X, mean2, dev2)*prior_S2, 'color', 'r');
+p3 = plot(S1, 0, '-bo');
+p4 = plot(S2, 0, '-ro');
+p5 = plot(T, 0, '-ks');
+legend([p1(1), p2(1), p3(1), p4(1), p5(1)], 'P(\omega_1)*p(x|\omega_1)', ... 
+    'P(\omega_2)*p(x|\omega_2)', 'S1', 'S2', 'T');
+title('Elements of S1, S2 and T');
+xlabel('S1, S2, T');
+ylabel('Probability');
+set(gca, 'fontsize', 8.5, 'fontname', 'Times New Roman');
+
+% 2.5
+syms x
+eqn = prior_S1*1/(sqrt(2*pi)*dev1)*exp(-0.5*(x-mean1)^2/dev1^2) == ...
+prior_S2*1/(sqrt(2*pi)*dev2)*exp(-0.5*(x-mean2)^2/dev2^2);
+solx = double(solve(eqn, x));
+for i=1:numel(solx)
+    fprintf('x(%i) = %f\n', i, solx(i));
+end
