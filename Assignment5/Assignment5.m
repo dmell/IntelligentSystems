37fad514fa8e4ef51087e8b4ee8f5f51f13833b5
dmell <damiano.melotti@studenti.unitn.it>
2018-10-12 18:03:40 +0200

    Today's work

--- Assignment5/Assignment5.m
+++ Assignment5/Assignment5.m
@@ -1,12 +1,8 @@
-P = 3; % number of examples
 % read the files
 load('w6_1x.mat');
-load('w6_1y.mat');
-load('w6_1z.mat');
 
-N = numel(w6_1x); % dimension of input vector
-
-K = 4; % number of prototypes
+K = 2; % number of prototypes
 nu = 0.1; % learning rate
-t_max = 3;
+t_max = 50;
 
+VQ(w6_1x, K, nu, t_max);
--- Assignment5/VQ.m
+++ Assignment5/VQ.m
@@ -1,35 +1,62 @@
-function vq = VQ (dataset, K, nu, t_max);
+function [] = VQ (dataset, K, nu, t_max)
+
+P = size(dataset, 1); % number of samples
+N = size(dataset, 2); % dimension of input vector
 
 % initial random permutation of the data set to get the prototypes
-dataset = dataset(randperm(size(dataset,1)),:);
+dataset = dataset(randperm(P),:);
 proto = dataset(1:K, :);
+
+% quantization error for every epoch
+H_VQ = zeros(1, t_max);
+
+% for every element we save the winner prototype
+dataWinner = zeros(1, P);
 
 % for every epoch
 for t=1:t_max
     % random permutation of the dataset
-    dataset = dataset(randperm(size(dataset,1)),:);
+    dataset = dataset(randperm(P),:);
     
     % for all elements
-    for i=1:size(dataset, 1)
-        % for all prototypes, find the closest
+    for i=1:P
         best = Inf;
-        winner = zeros(2);
+        winner = zeros(1, N);
+        index = 1;
+        % for all prototypes, find the closest
         for j=1:K
             % euclidean distance
-            current = (dataset(i, 1) - proto(j, 1))^2 + (dataset(i, 2) - proto(j, 2))^2;
+            current = pdist([dataset(i, :);proto(j, :)], 'squaredeuclidean');
             if (current < best)
                 best = current;
                 winner = proto(j, :);
+                index = j;
             end
         end
         
         % update the winner position
-        winner(1) = winner(1) + nu*(dataset(i,1)-winner(1));
-        winner(2) = winner(2) + nu*(dataset(i,2)-winner(2));
+        winner = winner + nu * (dataset(i,:) - winner);
+        % winner(1) = winner(1) + nu*(dataset(i,1)-winner(1));
+        % winner(2) = winner(2) + nu*(dataset(i,2)-winner(2));
+        proto(index, :) = winner;
+        
+        dataWinner(i) = index;
     end
     
+    % quantization error calculation
+    for i=1:P
+        H_VQ(t) = H_VQ(t) + pdist([dataset(i, :); proto(dataWinner(i), :)], 'squaredeuclidean');
+    end
 end
 
+figure(1);
 scatter(dataset(:,1), dataset(:,2), '.');
 hold on;
 scatter(proto(:, 1), proto(:, 2));
+
+figure(2);
+plot(1:t_max, H_VQ);
+title('Learning curve');
+xlabel('t');
+ylabel('H_{VQ}');
+set(gca, 'fontsize', 9.5, 'fontname', 'Times New Roman');
